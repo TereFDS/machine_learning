886f6443003303c8e0bdbbc65f55639b5eee2e22
Teresa F <tere.fds@hotmail.com>
2014-08-26 10:22:47 -0300

    changes

--- TP1/src/ENCONTRARS.m
+++ TP1/src/ENCONTRARS.m
@@ -2,12 +2,11 @@ function  hipotesis = ENCONTRARS( description,trainingSet )
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
     [rows, varQty]=size(description);
-    [trainQty, result]=size(trainingSet)
+    [trainQty, result]=size(trainingSet);
     nullVal=-1;
     generalVal=0;
     hipotesis=repmat(nullVal,1,varQty);
     for i=1:trainQty
-        disp(trainingSet(i,:));
         if(trainingSet(i,result)>0)
             for j=1:varQty
                if(hipotesis(j)~=generalVal && trainingSet(i,j)~=hipotesis(j))
@@ -19,8 +18,7 @@ function  hipotesis = ENCONTRARS( description,trainingSet )
                end
             end    
         end
-        disp('Hipotesis');
-        disp(hipotesis);
+        
     end
 end
 
